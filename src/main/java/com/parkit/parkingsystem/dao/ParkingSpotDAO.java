ae2b3e173f3d41f99917a07328a0ab423238f763
Oceane Lemee <oceane_lemee@hotmail.fr>
2020-02-29 17:53:32 +0100

    Commit
    .

--- src/main/java/com/parkit/parkingsystem/dao/ParkingSpotDAO.java
+++ src/main/java/com/parkit/parkingsystem/dao/ParkingSpotDAO.java
@@ -1,59 +1,79 @@
 package com.parkit.parkingsystem.dao;
 
-import com.parkit.parkingsystem.config.DataBaseConfig;
-import com.parkit.parkingsystem.constants.DBConstants;
-import com.parkit.parkingsystem.constants.ParkingType;
-import com.parkit.parkingsystem.model.ParkingSpot;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.parkit.parkingsystem.config.DataBaseConfig;
+import com.parkit.parkingsystem.constants.DBConstants;
+import com.parkit.parkingsystem.constants.ParkingType;
+import com.parkit.parkingsystem.model.ParkingSpot;
+
 public class ParkingSpotDAO {
-    private static final Logger logger = LogManager.getLogger("ParkingSpotDAO");
+	private static final Logger logger = LogManager.getLogger("ParkingSpotDAO");
 
-    public DataBaseConfig dataBaseConfig = new DataBaseConfig();
+	public DataBaseConfig dataBaseConfig = new DataBaseConfig();
 
-    public int getNextAvailableSlot(ParkingType parkingType){
-        Connection con = null;
-        int result=-1;
-        try {
-            con = dataBaseConfig.getConnection();
-            PreparedStatement ps = con.prepareStatement(DBConstants.GET_NEXT_PARKING_SPOT);
-            ps.setString(1, parkingType.toString());
-            ResultSet rs = ps.executeQuery();
-            if(rs.next()){
-                result = rs.getInt(1);;
-            }
-            dataBaseConfig.closeResultSet(rs);
-            dataBaseConfig.closePreparedStatement(ps);
-        }catch (Exception ex){
-            logger.error("Error fetching next available slot",ex);
-        }finally {
-            dataBaseConfig.closeConnection(con);
-        }
-        return result;
-    }
+	public int getRowsCountWithSameVehiculeNumber(String vehicleRegNumber) {
+		Connection con = null;
+		int result = 0;
+		try {
+			con = dataBaseConfig.getConnection();
+			PreparedStatement ps = con.prepareStatement(DBConstants.COUNT_RECURRENT_VEHICULE);
+			ps.setString(1, vehicleRegNumber);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				result = rs.getInt("count");
+			}
+			return result;
+		} catch (Exception ex) {
+			logger.error("Error fetching next available slot", ex);
+		} finally {
+			dataBaseConfig.closeConnection(con);
+		}
+		return result;
+	}
 
-    public boolean updateParking(ParkingSpot parkingSpot){
-        //update the availability fo that parking slot
-        Connection con = null;
-        try {
-            con = dataBaseConfig.getConnection();
-            PreparedStatement ps = con.prepareStatement(DBConstants.UPDATE_PARKING_SPOT);
-            ps.setBoolean(1, parkingSpot.isAvailable());
-            ps.setInt(2, parkingSpot.getId());
-            int updateRowCount = ps.executeUpdate();
-            dataBaseConfig.closePreparedStatement(ps);
-            return (updateRowCount == 1);
-        }catch (Exception ex){
-            logger.error("Error updating parking info",ex);
-            return false;
-        }finally {
-            dataBaseConfig.closeConnection(con);
-        }
-    }
+	public int getNextAvailableSlot(ParkingType parkingType) {
+		Connection con = null;
+		int result = -1;
+		try {
+			con = dataBaseConfig.getConnection();
+			PreparedStatement ps = con.prepareStatement(DBConstants.GET_NEXT_PARKING_SPOT);
+			ps.setString(1, parkingType.toString());
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()) {
+				result = rs.getInt(1);
+			}
+			dataBaseConfig.closeResultSet(rs);
+			dataBaseConfig.closePreparedStatement(ps);
+		} catch (Exception ex) {
+			logger.error("Error fetching next available slot", ex);
+		} finally {
+			dataBaseConfig.closeConnection(con);
+		}
+		return result;
+	}
 
+	public boolean updateParking(ParkingSpot parkingSpot) {
+		// update the availability for that parking slot
+		Connection con = null;
+		try {
+			con = dataBaseConfig.getConnection();
+			PreparedStatement ps = con.prepareStatement(DBConstants.UPDATE_PARKING_SPOT);
+			ps.setBoolean(1, parkingSpot.isAvailable());
+			ps.setInt(2, parkingSpot.getId());
+			int updateRowCount = ps.executeUpdate();
+			dataBaseConfig.closePreparedStatement(ps);
+			return (updateRowCount == 1);
+		} catch (Exception ex) {
+			logger.error("Error updating parking info", ex);
+			return false;
+		} finally {
+			dataBaseConfig.closeConnection(con);
+		}
+	}
 }
--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -1,31 +1,42 @@
 package com.parkit.parkingsystem.service;
 
 import com.parkit.parkingsystem.constants.Fare;
+import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.model.Ticket;
 
 public class FareCalculatorService {
 
-    public void calculateFare(Ticket ticket){
-        if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
-            throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
-        }
+	ParkingSpotDAO parkingSpotDAO = new ParkingSpotDAO();
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+	public void calculateFareByType(Ticket ticket, double fareType) {
+		double diff = ticket.getOutTime().getTime() - ticket.getInTime().getTime();
+		double duration = diff / 1000;
+		int freeTimeInSeconds = 1800;
 
-        //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+		if (parkingSpotDAO.getRowsCountWithSameVehiculeNumber(ticket.getVehicleRegNumber()) > 0) {
+			ticket.setPrice((duration * fareType) * (5 / 100));
+		} else if (duration <= freeTimeInSeconds) {
+			ticket.setPrice(0 * fareType);
+		} else {
+			ticket.setPrice(duration * fareType);
+		}
+	}
 
-        switch (ticket.getParkingSpot().getParkingType()){
-            case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
-                break;
-            }
-            case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
-                break;
-            }
-            default: throw new IllegalArgumentException("Unkown Parking Type");
-        }
-    }
+	public void calculateFare(Ticket ticket) {
+		if ((ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime()))) {
+			throw new IllegalArgumentException("Out time provided is incorrect:" + ticket.getOutTime().toString());
+		}
+		switch (ticket.getParkingSpot().getParkingType()) {
+		case CAR: {
+			calculateFareByType(ticket, Fare.CAR_RATE_PER_SECOND);
+			break;
+		}
+		case BIKE: {
+			calculateFareByType(ticket, Fare.BIKE_RATE_PER_SECOND);
+			break;
+		}
+		default:
+			throw new IllegalArgumentException("Unkown Parking Type");
+		}
+	}
 }
